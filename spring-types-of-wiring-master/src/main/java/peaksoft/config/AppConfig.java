fb494affb68fa8d25c8a6fa153c246263469458e
aidai <izumisoft1@mail.ru>
2023-01-18 21:40:45 +0900

    Initial commit

---
+++ spring-types-of-wiring-master/src/main/java/peaksoft/config/AppConfig.java
@@ -0,0 +1,45 @@
+package peaksoft.config;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import peaksoft.model.*;
+
+@Configuration
+@ComponentScan(basePackages = "peaksoft")
+public class AppConfig {
+//    @Bean
+//    public static Ocean1 ocean1(Island2 island2){
+//        return new Ocean1(island2);
+//    }
+//    @Bean
+//    public static Island2 getIsland( Wood3 wood) {
+//        return new Island2(wood);
+//    }
+//
+//    @Bean
+//    public static Wood3 getWood3(Rabbit4 rabbit4){
+//        return new Wood3(rabbit4);
+//    }
+//
+//    @Bean
+//    public static Rabbit4 getRabbit4(Duck5 duck5){
+//        return new Rabbit4(duck5);
+//    }
+//    @Bean
+//    public static Duck5 getDuck( Egg6 egg){
+//        return new Duck5(egg);
+//    }
+//    @Bean
+//    public static Egg6 getEgg( Needle7 needle){
+//        return new Egg6(needle);
+//    }
+//    @Bean
+//    public static Needle7 getNeedle(Deth8 deth){
+//        return new Needle7(deth);
+//    }
+
+}
---
+++ spring-types-of-wiring-master/src/main/java/peaksoft/model/Deth8.java
@@ -0,0 +1,13 @@
+package peaksoft.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import peaksoft.KoscheiTheDeathless;
+
+@Component
+public class Deth8 {
+    @Override
+    public String toString() {
+        return "смерть Кощея";
+    }
+}
